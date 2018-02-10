c72056ec919e16211e0efc95c706658108e9f52d
Brian Yu <brianyu28@gmail.com>
2018-02-10 11:18:28 -0500

    Update books

--- application.py
+++ application.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, session
+from flask_session import Session
 
 app = Flask(__name__)
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.route("/")
 def index():
